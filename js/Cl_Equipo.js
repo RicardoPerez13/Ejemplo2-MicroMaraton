b9939735b086b6cdf71451a81cc763ad931d2d2e
RicardoPerez13 <1001.30995733.ucla@gmail.com>
2024-07-10 22:51:25 -0400

    commit ¨(feature) Inicio del código

--- js/Cl_Equipo.js
+++ js/Cl_Equipo.js
@@ -1,3 +1,5 @@
 export default class Cl_Equipo {
-
+constructor (){
+    
+}
 }
--- js/Cl_Partido.js
+++ js/Cl_Partido.js
@@ -1,3 +1,11 @@
 export default class Cl_Partido{
-    
+    constructor(result){
+        this.resultado=result;
+        }
+        set resultado (r) {
+            this._resultado=r;
+    }
+    get resultado () {
+      return this._resultado;
+    }
 }
